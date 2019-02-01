c5a46d5e2a5033ddfc9e9a7722cdea0bd3a45661
mdashl <mdashlw@gmail.com>
2019-02-01 16:26:13 +0300

    Migrate to Gradle Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.20"
+    `maven-publish`
+}
+
+group = "com.github.mdashl"
+version = "2.0"
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
+    implementation("net.dv8tion:JDA:4.ALPHA.0_27") {
+        exclude(module = "opus-java")
+    }
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven("https://gitlab.com/api/v4/projects/10590152/packages/maven") {
+            credentials(HttpHeaderCredentials::class) {
+                name = "Job-Token"
+                value = System.getenv("CI_JOB_TOKEN")
+            }
+
+            authentication {
+                create<HttpHeaderAuthentication>("header")
+            }
+        }
+    }
+}
